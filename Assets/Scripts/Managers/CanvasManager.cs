540f93df59fbcc03861284183c8fe216207926ac
matteorusso27 <matteo2795@gmail.com>
2024-03-24 09:12:34 +0100

    added debugger in canvas

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -6,10 +6,16 @@ public class CanvasManager : Singleton<CanvasManager>
 {
     public TMP_Text tmpPro;
     public Image fillBar;
+    public TMP_Text swipeState;
 
     public void SetText(string text)
     {
         tmpPro.text = text;
+    }
+
+    public void SetSwipeStateText(string text)
+    {
+        swipeState.text = text;
     }
 
     public void SetFillBar(float value)
