bfc7691ca72cd7383dfc1a919f609fb98c3d0faf
matteorusso27 <matteo2795@gmail.com>
2024-03-24 20:17:00 +0100

    new tags in game and helpers update

--- Assets/Scripts/Utilities/Helpers.cs
+++ Assets/Scripts/Utilities/Helpers.cs
@@ -5,12 +5,14 @@ using UnityEngine;
 public static class Helpers
 {
     public static float TIME_TO_SWIPE = 1.5f;
-    public static float PLAYER_TURN_TIME = 5f;
+    public static float PLAYER_TURN_TIME = 25f;
     public static float GRAVITY = 9.8f;
+    public static Vector3 HOOP_POSITION = new Vector3(0.25f, 8.732f, 5.43f);
 
     public enum GameTag
     {
-        Terrain
+        Terrain,
+        Board
     }
 
     public static string StringTag(GameTag t) => t.ToString();
